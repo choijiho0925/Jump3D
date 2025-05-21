426e89a75dbfdcbd076d46801278101d1f52dc6d
choijiho0925 <jiho.0925.choi@gmail.com>
2025-05-21 18:35:17 +0900

    feat : 점프대 구현

---
+++ Assets/Scripts/Object/JumpingPlatform.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class JumpingPlatform : MonoBehaviour
+{
+    [Header("Jump")]
+    public float jumpForce; // 점프 힘
+    public float forwardMultiplier; // 밀어주는 힘
+
+    public LayerMask playerLayer;
+
+    private Rigidbody rb;
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (((1 << collision.gameObject.layer) & playerLayer) == 0) return;
+        
+        Rigidbody rb = collision.attachedRigidbody; // 플레이어 rigidbody 가져오기
+
+        Vector3 horizontalDir = new Vector3(rb.velocity.x, 0f, rb.velocity.z).normalized; // 현재 속도에서 수평 방향 추출
+        Vector3 jumpBoost = (horizontalDir * forwardMultiplier) + (Vector3.up * jumpForce); // 점프 힘 계산 
+
+        rb.AddForce(jumpBoost, ForceMode.VelocityChange); // 기존 속도에 추가 힘 적용 (VelocityChange는 즉시 속도 변화를 만듬)
+    }
+}
+
